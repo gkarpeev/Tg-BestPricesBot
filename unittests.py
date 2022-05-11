06f6e267bcf727ba344e19af4e74f141299272fc
gkarpeev <glebkarpeev046@gmail.com>
2022-05-11 18:23:00 +0300

    Edited Unit Tests

--- unittests.py
+++ unittests.py
@@ -4,24 +4,35 @@ import config
 import parse_sber
 import parse_aliexpress
 
+
 class TestBot(unittest.TestCase):
     def test_connection(self):
-        self.assertEqual(requests.get('https://sbermegamarket.ru/').status_code, 200)
-        self.assertEqual(requests.get('https://aliexpress.ru/').status_code, 200)
+        self.assertEqual(
+            requests.get('https://sbermegamarket.ru/').status_code, 200)
+        self.assertEqual(
+            requests.get('https://aliexpress.ru/').status_code, 200)
 
     def test_sber_parser(self):
-        self.assertFalse(parse_sber.findProduct("телефон")["cost"] == None)
-        self.assertFalse(parse_sber.findProduct("mac book")["cost"] == None)
-        self.assertFalse(parse_sber.findProduct("клавиатура logitech")["cost"] == None)
-        self.assertTrue(len(parse_sber.findProduct("телефон")) == 3)
+        self.assertFalse(
+            parse_sber.findProduct("телефон")["cost"] is None)
+        self.assertFalse(
+            parse_sber.findProduct("mac book")["cost"] is None)
+        self.assertFalse(
+            parse_sber.findProduct("клавиатура logitech")["cost"] is None)
+        self.assertTrue(
+            len(parse_sber.findProduct("телефон")) == 3)
 
-    
     def test_ali_parser(self):
-        self.assertFalse(parse_aliexpress.findProduct("телефон")["cost"] == None)
-        self.assertFalse(parse_aliexpress.findProduct("ноутбук")["cost"] == None)
-        self.assertFalse(parse_aliexpress.findProduct("компьютерная мышь")["cost"] == None)
-        self.assertFalse(parse_aliexpress.findProduct("клавиатура")["cost"] == None)
-        self.assertTrue(len(parse_aliexpress.findProduct("телефон")) == 3)
+        self.assertFalse(
+            parse_aliexpress.findProduct("телефон")["cost"] is None)
+        self.assertFalse(
+            parse_aliexpress.findProduct("ноутбук")["cost"] is None)
+        self.assertFalse(
+            parse_aliexpress.findProduct("компьютерная мышь")["cost"] is None)
+        self.assertFalse(
+            parse_aliexpress.findProduct("клавиатура")["cost"] is None)
+        self.assertTrue(
+            len(parse_aliexpress.findProduct("телефон")) == 3)
 
 
 if __name__ == "__main__":
